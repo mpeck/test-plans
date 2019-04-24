07a35c1f7ebc600393e9c660ca4da0a24815be20
Matthew Peck <mpeck@chef.io>
2019-04-24 17:22:20 -0400

    add node
    Signed-off-by: Matthew Peck <mpeck@chef.io>

---
+++ node/plan.ps1
@@ -0,0 +1,29 @@
+$pkg_name="node"
+$pkg_origin="core"
+$pkg_version="11.13.0"
+$pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
+$pkg_upstream_url="https://nodejs.org/"
+$pkg_license=@("MIT")
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
+$pkg_shasum="8d0963f62059526146ffbd2a4fba36276399fcb5580ba7f1def8cc2b4a35fe76"
+$pkg_build_deps=@("core/lessmsi")
+$pkg_bin_dirs=@("bin")
+
+function Invoke-Unpack {
+  mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+
+  Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  try {
+    lessmsi x (Resolve-Path "$HAB_CACHE_SRC_PATH/$pkg_filename").Path
+  }
+  finally { Pop-Location }
+}
+
+function Invoke-Install {
+  Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/node-v$pkg_version-x64/SourceDir/nodejs/*" "$pkg_prefix/bin" -Recurse
+}
+
+function Invoke-Check() {
+  (& "$HAB_CACHE_SRC_PATH/$pkg_dirname/nodejs/node" --version) -eq "v$pkg_version"
+}
